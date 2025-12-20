1b43911617c5b0d0579e5c693d9d6017ed2b25ef
Joel Bijo <joelbijo05@gmail.com>
2025-12-20 11:44:02 +0530

    Remove clutter
    Removed card opening and closing functionality.

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //Play bday song on window load
 window.addEventListener("load", function () {
   const song = document.getElementById("bdaySong");
-  song.play().catch((err) => {
-    console.log("Autoplay stopped.");
-  });
 });
 
 //Blow out candle
@@ -11,20 +8,8 @@ const blow = document.getElementById("blowCandleBtn");
 const flame = document.getElementById("candle-flame");
 blow.addEventListener("click", function () {
   flame.classList.add("hidden");
-  blow.textContent = "Make a wish! ✨";
+  blow.textContent = "Make a wish! ";
   blow.disabled = true;
   blow.style.opacity = "0.6";
   blow.style.cursor = "not-allowed";
-});
-
-//Card
-const openCard = document.getElementById("openCardBtn");
-const greetingCard = document.getElementById("greeting-card");
-const closeCard = document.getElementById("closeCardBtn");
-openCard.addEventListener("click", function () {
-  greetingCard.classList.add("show");
-});
-
-closeCard.addEventListener("click", function () {
-  greetingCard.classList.remove("show");
 });
